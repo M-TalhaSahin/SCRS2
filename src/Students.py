64163945ac43488600a1c18bf5bbbe3d88cdf98b
Hüseyin Can Ergün <hcanergun60@gmail.com>
2023-05-09 20:40:15 +0300

    file to json / load from json
    gerekli fonksiyonlar eklendi

--- src/Students.py
+++ src/Students.py
@@ -1,3 +1,4 @@
+import json
 
 class Students:
     def __init__(self):
@@ -9,18 +10,58 @@ class Students:
         self.studentsDict[studentId].addSingleCourse(courseId, period, grade, credit)
 
     def toJson(self, fileName: str):
-        pass
+        jsonDict = {}
+        studentList = []
+        singleStudentDict = {}
+        semesterList = []
+        singleSemesterDict = {}
+        courseList = []
+        courseDict = {}
+
+        for student in self.studentsDict.keys():
+            singleStudentDict.clear()
+            singleStudentDict["studentId"]= self.studentsDict[student].studentId
+            semesterList.clear()
+            for semester in self.studentsDict[student].semesterDict.keys():
+                singleSemesterDict.clear()
+                p = tuple(map(int, self.studentsDict[student].semesterDict[semester].period.replace('(', '').replace(')', '').split(', ')))
+                singleSemesterDict["year"] = p[0]
+                singleSemesterDict["period"] = p[1]
+                courseList.clear()
+                for course in self.studentsDict[student].semesterDict[semester].course_grade_list:
+                    courseDict.clear()
+                    courseDict["courseName"] = course[0]
+                    courseDict["credit"] = course[2]
+                    courseDict["grade"] = course[1]
+                    courseList.append(courseDict.copy())
+
+                singleSemesterDict["courseGradeList"] = courseList.copy()
+                semesterList.append(singleSemesterDict.copy())
+
+            singleStudentDict["semesters"] = semesterList.copy()
+            studentList.append(singleStudentDict.copy())
+
+        jsonDict["students"] = studentList.copy()
+
+        with open(fileName, 'w') as f:
+            json.dump(jsonDict, f, indent=4, ensure_ascii = False)
 
     @classmethod
     def loadFromJson(cls, fileName: str):
 
-        """
-        https://www.geeksforgeeks.org/what-is-a-clean-pythonic-way-to-have-multiple-constructors-in-python/
+        with open(fileName, 'r') as f:
+            jsonDict = json.load(f)
 
-        kullanırken
-        myStudents = Students.loadFromJson("file.json")
-        """
-        pass
+        students = cls()
+
+        for studentDict in jsonDict['students']:
+            for semesterDict in studentDict['semesters']:
+                for courseDict in semesterDict['courseGradeList']:
+                    students.addSingleCourse(studentDict['studentId'], courseDict['courseName'],
+                                             str((int(semesterDict['year']), int(semesterDict['period']))),
+                                             courseDict['grade'], courseDict['credit'])
+
+        return students
 
 
 class Student:
@@ -49,3 +90,21 @@ class Semester:
     def __init__(self, _period: (int, int)):
         self.period = _period
         self.course_grade_list = []  # (name: str, credit: int, grade: float)
+
+
+if __name__ == "__main__":
+    s = Students.loadFromJson("data/trainStudents.json")
+    s.toJson("data/testing.json")
+
+    with open('data/testing.json', 'r') as f:
+        # Read the entire file as a string
+        prints = f.read()
+
+    with open('data/trainStudents.json', 'r') as f:
+        # Read the entire file as a string
+        old = f.read()
+
+    if prints == old:
+        print("eşit")
+    else:
+        print("farklı")
